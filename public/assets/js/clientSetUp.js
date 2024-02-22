eca01a918d412e9f63e4ada7a2bd510a782bc03a
Cristian <Crmueller1@users.noreply.github.com>
2024-02-22 12:23:58 -0600

    Office Form Still need to figure out error annotation on phone fields to only show on relevant field.

--- public/assets/js/clientSetUp.js
+++ public/assets/js/clientSetUp.js
@@ -11,4 +11,34 @@ $('#submitButton').on('click', function(event) {
   }
 });
 
+
+var $mailingAddressLines = $("#mailingAddressLine1, #mailingAddressLine2, #mailingAddressLine3, #mailingAddressLine4");
+$('#sameAsPhysicalLocation').change(function(event) {
+  event.preventDefault();
+  if ($(this).is(":checked")) {
+    $mailingAddressLines.addClass('hidden');
+  } else {
+    $mailingAddressLines.removeClass('hidden');
+  }
+});
+
+document.getElementById('primaryOfficeLine').addEventListener('blur', formatPhoneNumber);
+document.getElementById('secondaryBackLine').addEventListener('blur', formatPhoneNumber);
+document.getElementById('tollFree').addEventListener('blur', formatPhoneNumber); 
+
+function formatPhoneNumber (event) {
+  var phoneNumberInput = event.target;
+  var phoneNumber = phoneNumberInput.value.replace(/\D/g, ''); // Remove non-numeric characters
+
+  if (phoneNumber.length === 10) {
+      phoneNumberInput.value = '(' + phoneNumber.substring(0, 3) + ') ' + phoneNumber.substring(3, 6) + '-' + phoneNumber.substring(6);
+      document.getElementById('error-message').style.display = 'none'; // Hide error message if previously shown
+  } else {
+      // If the length is not 10 digits, display error message
+      document.getElementById('error-message').style.display = 'block';
+  }
+};
+
+
+
 console.log(getQuery[getQuery.length-1]);
