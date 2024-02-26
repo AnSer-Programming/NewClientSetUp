2bce55206f67b67cb40388b07285bfb405b624b5
Stephen Merki <stephen.merki232@gmail.com>
2024-02-26 16:33:47 -0600

    got NodeMailer set up to send an email on demand.

--- routes/clientSetUp.js
+++ routes/clientSetUp.js
@@ -1,11 +1,18 @@
 const clientSetUp = require('express').Router();
+const sendEmail = require('../nodeMailer/sendEmail');
 let uniqueURL;
+
+clientSetUp.get('/', (req, res) => {
+  sendEmail();
+  res.json("Test");
+});
 
 function getRandom() {
   const max = 9;
   const min = 0;
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
+  // sendEmail;
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
